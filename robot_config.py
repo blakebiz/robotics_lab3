a6135718d9e39f2dae2edce1690967e1c213f2b2
blakebiz <blakeblackport@gmail.com>
2022-02-28 01:12:50 +0000

    Fixed order of args

--- robot_config.py
+++ robot_config.py
@@ -5,7 +5,7 @@ from robot_model import *
 
 def test_a():
     # perform dh transformation on args given in assignment
-    result = dh_transformation(1, 1, math.pi / 2, math.pi / 2)
+    result = dh_transformation(1, math.pi / 2, 1, math.pi / 2)
     # return the position and rotation of the result
     return get_pos(result), get_rot(result)
 
@@ -25,13 +25,35 @@ def test_b():
     # return position and rotation of result
     return get_pos(result), get_rot(result)
 
+def test_c():
+    test_cases = [
+        [math.pi / 2, .1625, 0, 0],
+        [0, -.425, -math.pi / 2, 0],
+        [0, -.3922, 0, 0],
+        [math.pi / 2, 0, 0, .1333],
+        [-math.pi / 2, 0, 0, .0997],
+        [0, 0, 0, .0996]
+    ]
+    # get kinematic chain result of the test cases
+    result = kinematic_chain(test_cases)
+    # return position and rotation of result
+    return get_pos(result), get_rot(result)
+
+
+def foo():
+    import itertools
+    for perm in itertools.permutations([1, math.pi / 2, 1, math.pi / 2]):
+        print(perm, test_a(*perm))
+
 
 if __name__ == '__main__':
     # call the two test functions and print the results
     test1 = test_a()
     print(f'test a: {test1}')
     test2 = test_b()
-    print(f'test b: {test2}')
+    print(f'test b1: {test2}')
+    test3 = test_c()
+    print(f'test b2: {test3}')
 
     # for some reason test b appears to work fine (well mostly one or two numbers are very slightly off but I'm assuming
     # that's due to float imprecision)
